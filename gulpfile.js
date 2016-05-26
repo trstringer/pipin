04ad742f38494601dd7d6142a5e89e55480694ac
Thomas Stringer <tr.stringer@outlook.com>
2016-05-26 18:16:10 -0400

    Active development

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 
 gulp.task('test', function () {
-  exec('node index.js --list', function (err, stdout, stderr) {
+  exec('node index.js -m rpiab', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   });
--- model-output.js
+++ model-output.js
@@ -1,27 +1,76 @@
+const config = {
+  leftMarginSpacing: 6
+};
+
 function displayModelName(modelDetails) {
   if (modelDetails.name) {
     console.log(`Model: ${modelDetails.name}`);
+    console.log(' ');
   }
+}
+
+function displaySchematicHeaderFooter() {
+  var output = '';
+  var i;
+  for (i = 0; i < config.leftMarginSpacing + 1; i++) {
+    output += ' ';
+  }
+  
+  console.log(`${output}+-------+`);
+}
+
+function displayPinRow(pinLeft, pinRight) {
+  var displayRow;
+  
+  displayRow = formatPinForOutput(pinLeft, true);
+  displayRow += ' | o | o | ';
+  displayRow += formatPinForOutput(pinRight, false);
+  
+  console.log(displayRow);
+}
+
+function formatPinForOutput(pin, isLeft) {
+  // the format is:
+  //  - 6 char for <pin-spec>
+  //  - left pad left pins <pin-spec> with spaces
+  var pinSpecDisplay;
+  switch (pin.type) {
+    case 'constantPower':
+      pinSpecDisplay = isLeft ? leftPad(`${pin.voltage}V`, ' ', config.leftMarginSpacing) : `${pin.voltage}V`;
+      break;
+      
+    case 'ground':
+      pinSpecDisplay = isLeft ? leftPad('GND', ' ', config.leftMarginSpacing) : 'GND';
+      break;
+      
+    case 'gpio':
+      pinSpecDisplay = isLeft ? leftPad(`GPIO${pin.gpioId}`, ' ', config.leftMarginSpacing) : `GPIO${pin.gpioId}`;
+      break;
+  }
+  return pinSpecDisplay;
+}
+
+function leftPad(input, padChar, padLength) {
+  var i;
+  var tempOutput = '';
+  for (i = 0; i < padLength; i++) {
+    tempOutput += padChar;
+  }
+  
+  tempOutput += input;
+  return tempOutput.substring(tempOutput.length - padLength);
 }
 
 function displayPins(modelDetails) {
   if (modelDetails.pins && modelDetails.pins.length > 0) {
+    displaySchematicHeaderFooter();
+    
     var i;
-    for (i = 0; i < modelDetails.pins.length; i++) {
-      switch (modelDetails.pins[i].type) {
-        case 'constantPower':
-          console.log(`Physical pin ${modelDetails.pins[i].physicalId}, voltage ${modelDetails.pins[i].voltage}V`);
-          break;
-          
-        case 'ground':
-          console.log(`Physical pin ${modelDetails.pins[i].physicalId}, GROUND`);
-          break;
-          
-        case 'gpio':
-          console.log(`Physical pin ${modelDetails.pins[i].physicalId}, GPIO ${modelDetails.pins[i].gpioId}`);
-          break;
-      }
+    for (i = 0; i < modelDetails.pins.length; i += 2) {
+      displayPinRow(modelDetails.pins[i], modelDetails.pins[i + 1]);
     }
+    
+    displaySchematicHeaderFooter();
   }
 }
 
